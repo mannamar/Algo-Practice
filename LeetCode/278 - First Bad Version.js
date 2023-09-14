e45286b5faaf40a7bc36503396009ac48c028d06
Amardeep Mann <amar2slick@gmail.com>
2023-09-14 02:13:17 -0700

    Solved without hash table

--- LeetCode/278 - First Bad Version.js
+++ LeetCode/278 - First Bad Version.js
@@ -4,36 +4,21 @@
 var solution = function(isBadVersion) {
 
     return function(n) {
-        let hash = {};
         let start = 1;
         let end = n;
-        if (end === 1) {
-            return end;
-        }
-        // Come up with proper stop condition
-        while (true) {
+        while (start < end) {
             let middle = Math.floor((start + end)/2);
-            console.log(middle);
-            let isMidBad = isBadVersion(middle);
-            if (isMidBad) {
-                if (hash[middle - 1] === false) {
-                    return middle;
-                }
-                hash[middle] = isMidBad;
-                end = middle - 1;
+            if (isBadVersion(middle)) {
+                end = middle;
             } else {
-                if (hash[middle + 1] === true) {
-                    return middle + 1;
-                }
-                hash[middle] = isMidBad;
                 start = middle + 1;
             }
         }
+        return start;
     };
 
 };
 
-// Solved with good speed
-    // Space complexity is very poor O(n)
-        // Should be solvable without hash table
-        // Would reduce space to O(1)
+// Solved with good speed and space
+// Learn explanation for why new end is middle inclusive
+    // But new start is middle exclsuive
