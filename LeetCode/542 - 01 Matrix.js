88c9df676abcb424aaa0a1c516e6eaab3f8ab05e
Amardeep Mann <amar2slick@gmail.com>
2023-09-19 01:20:29 -0700

    Pseudocoded possible solution

---
+++ LeetCode/542 - 01 Matrix.js
@@ -0,0 +1,22 @@
+// 542. 01 Matrix
+// https://leetcode.com/problems/01-matrix/
+
+var updateMatrix = function(mat) {
+    // Create result matrix
+    // Iterate over columns
+        // Iterate over rows
+            // Find nearest zero (call findNearestZero)
+            // Set returned value in result array
+    // Return result array
+};
+
+function findNearestZero(mat, m, n) {
+    // Initialize counter
+    // While 0 not found
+        // Search counter spaces away for a 0
+    // Return counter
+}
+
+// Will likely need to expand upon "Search counter spaces away for a 0"
+    // Do you need to check diagonals? Or just up, down, left, right? 
+// As outlined time complexity would be O(m^2 * n^2)?
