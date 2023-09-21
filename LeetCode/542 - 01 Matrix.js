6a5f990b85cd08f5b111357647966d8059c207bf
Amardeep Mann <amar2slick@gmail.com>
2023-09-20 19:53:32 -0700

    Solved

--- LeetCode/542 - 01 Matrix.js
+++ LeetCode/542 - 01 Matrix.js
@@ -1,13 +1,25 @@
 // 542. 01 Matrix
 // https://leetcode.com/problems/01-matrix/
 
-// New approach - Using queue for efficency 
+// New approach - BFS plus using queue for efficency 
 
 var updateMatrix = function(mat) {
-    // Initialize empty queue array
+    // Initialize empty queue
+    let queue = [];
+
     // Iterate over matrix
         // If value is zero add coordinates to queue
         // Else replace cell value with -1
+    for(let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat[0].length; j++) {
+            if (mat[i][j] === 0) {
+                queue.push([i,j]);
+            } else {
+                mat[i][j] = -1;
+            }
+        }
+    }
+
     // While queue contains coords
         // Set current coords to first value from queue
         // Get value of current coords (initial neighbor value)
@@ -17,19 +29,35 @@ var updateMatrix = function(mat) {
             // If coords in matrix range AND value is -1
                 // Set value to 1 + initial neighbor value
                 // Push these new coords to the queue to check its neighbors
+    while (queue.length > 0) {
+        let [row, col] = queue.shift();
+        let val = mat[row][col];
+        let coords = [ [row + 1, col] , [row, col + 1] , [row - 1, col] , [row, col - 1] ];
+        for (let i = 0; i < coords.length; i++) {
+            let [newRow, newCol] = coords[i];
+            if (newRow >= 0 && newCol >= 0 && newRow < mat.length && newCol < mat[0].length && mat[newRow][newCol] === -1) {
+                mat[newRow][newCol] = val + 1;
+                queue.push([newRow, newCol]);
+            }
+        }
+    }
+    
     // Return result array
+    return mat;
 };
 
 // Breadth First Search (BFS)
     // Start at zeroes (instead of 1's) and move outward
+    // Queue so you only need to check next level and not entire matrix
     // Results in O(m*n) time complexity
         // Each cell of the matrix is only ever enqueued once
-// Brain dead, code later
+
 
 
 
 // Previous incorrect approach
-// Diagonals DO NOT count as one step
+    // Diagonals DO NOT count as one step
+    // Also very inefficient
 
 var updateMatrix = function(mat) {
     // Create result matrix
@@ -51,6 +79,7 @@ function findNearestZero(mat, m, n) {
     // Initialize counter
     // While 0 not found
         // Search counter spaces away for a 0
+        // Increment counter
     // Return counter
     let counter = 0;
     while(true) {
