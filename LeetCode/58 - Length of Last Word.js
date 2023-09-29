b563ff2ce9cc99f8de52643c063f1b6464a52dbe
Amardeep Mann <amar2slick@gmail.com>
2023-09-28 22:18:37 -0700

    Alt solution and notes

--- LeetCode/58 - Length of Last Word.js
+++ LeetCode/58 - Length of Last Word.js
@@ -2,6 +2,7 @@
 // https://leetcode.com/problems/length-of-last-word/
 
 
+// Using for loop
 var lengthOfLastWord = function(s) {
     // Initialize result
     let length = 0;
@@ -24,6 +25,27 @@ var lengthOfLastWord = function(s) {
 }
 
 
+// Using built-in methods
+var lengthOfLastWord = function(s) {
+    // Trim white space
+    s = s.trim();
+
+    // Get index of the last space
+    let idxLastSpace = s.lastIndexOf(' ');
+
+    // Return length of last word
+    return s.length - 1 - idxLastSpace;
+}
+
+
+// NOTES
 // Optimal solution starts at end of string
+    // Naive solution iterates from start of string
     // Naive and optimal are both O(n) due to worst-case
-    // But optimal will generally be much faster for long strings
+    // But optimal will on average be much faster - especially for long strings
+// When using built-in methods avoid using .split() if we don't need to traverse the entire string
+    // Understand exactly what a built-in methods does when called
+    // .split() will always traverse the entire string
+    // .lastIndexOf(char) will start at the end and traverse until char is found
+        // If given sane restraints for word length then .lastIndexOf(' ') runs at O(wordLength) => O(1) time
+        // While .split(s) will always run at O(n) time
