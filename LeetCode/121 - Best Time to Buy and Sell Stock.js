b1e52157d54c2f57a4d49dca0b2e5555e46d5cc9
Amardeep Mann <amar2slick@gmail.com>
2023-10-19 19:52:43 -0700

    Added notes and pseudocode

--- LeetCode/121 - Best Time to Buy and Sell Stock.js
+++ LeetCode/121 - Best Time to Buy and Sell Stock.js
@@ -4,20 +4,32 @@
 
 // Using sliding window
 var maxProfit = function(prices) {
+    // Set initial "buy" pointer
     let left = 0;
+    // Set initial "sell" pointer
     let right = 1;
+    // Initialize max profit as 0
     let maxProfit = 0;
 
+    // While right pointer is in bounds
     while (right < prices.length) {
+        // If the buy price is less than the sell price
         if (prices[left] < prices[right]) {
+            // Calculate the profit
             let profit = prices[right] - prices[left];
+            // If the profit is higher than the current max profit
+                // Set max profit to current profit
             maxProfit = Math.max(profit, maxProfit);
+        // Else (if buy price is equal to or greater than sale price)
         } else {
+            // Set left pointer to right position
             left = right;
         }
+        // Increment right pointer
         right++;
     }
 
+    // Return max profit
     return maxProfit;
 };
 
@@ -44,3 +56,8 @@ var maxProfit = function(prices) {
     }
     return maxProfit;
 };
+
+// NOTES
+// Both methods are O(n) time and O(1) space complexity
+// In essence they are doing the same thing
+    // Sliding window is more intuitive to follow
