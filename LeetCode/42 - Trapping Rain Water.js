d819df61199ca42e45971b0c17f79c28ccff24bf
Amardeep Mann <amar2slick@gmail.com>
2024-08-09 21:48:23 -0700

    Attempt 42 (Hard)

---
+++ LeetCode/42 - Trapping Rain Water.js
@@ -0,0 +1,42 @@
+// 42. Trapping Rain Water
+// https://leetcode.com/problems/trapping-rain-water/
+
+// WIP Solution
+var trap = function(height) {
+    // Initialize variables
+    let left = 0;
+    let right = 1;
+    let leftHeight = height[0];
+    let total = 0;
+
+    // While starting pointer has not reached end
+    while (left < height.length) {
+        // While value at end is less than or equal value at start
+        while (height[right] < height[left]) {
+            right++;
+            // If end goes out of bounds
+            if (right === height.length) {
+                left++;
+                leftHeight = height[left];
+                right = left + 1;
+            }
+        }
+        // While start is less than end
+        while (left < right) {
+            let water = leftHeight - height[left];
+            if (water > 0) {
+                total += water;
+            }
+            left++;
+        }
+        leftHeight = height[left];
+        right++;
+    }
+    return total;
+};
+
+// Passes first 145 test cases
+// Fails on test case 146: [4,2,3]
+    // Not currently accounting properly for right height
+    // Currently making the assumption there can not be water trapped when right walls are lower than current wall
+    // Likely need to set right pointer to end and work towards the middle
